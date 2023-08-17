5bb2c3148dcb940ead4bc9e56f37f4b687afe8e9
hannahkamii <hannahwacheke520@gmail.com>
2023-08-17 03:17:11 -0700

    hannah

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements
+ * @h: the main
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int data;
+	data = 0;
+
+	if (h == NULL)
+		return (data);
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		data++;
+		h = h->next;
+	}
+	return (data);
+}
